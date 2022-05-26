0c9ee0c3727e8b7c157ac06d274fe4b4ca27a96f
a2-ito <hi.mound@gmail.com>
2022-05-26 22:46:49 +0900

    clean up main.go

--- src/main.go
+++ src/main.go
@@ -1,18 +1,10 @@
 package main
 
 import (
-  //"context"
-  //"fmt"
   inf "src/infrastructure"
 )
 
 func main() {
   inf.Run()
-  /*
-  //ctx := context.Background()
-  url := conf.AuthCodeURL("state")
-  fmt.Printf("Visit the URL for the auth dialog: %v", url)
-  */
-
 }
 
